04c9ac035dfdaf17075a070e3ca04626ef8c4f36
mike giroux <michaelgiroux909@gmail.com>
2023-01-09 16:13:17 -0800

    commit tree 716cf582f484a7c7aa40d88ff37740c61ec3aa82

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(string){
+    return cats.push(string);
+}
+function destructivelyPrependCat(string){
+    return cats.unshift(string);
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function appendCat(string){  
+    const newArray = [...cats];
+    newArray.push(string);
+    return newArray;
+}
+function prependCat(string){
+    const newArray = [... cats];
+    newArray.unshift(string);
+    return newArray;
+}
+function removeLastCat(){
+    const newArrray = [... cats];
+    newArrray.pop();
+    return newArrray;
+}
+function removeFirstCat(){
+    const newArrray = [... cats];
+    newArrray.shift();
+    return newArrray;
+}
